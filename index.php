0f49f52b0d3ceaef1b9e7df4f985978fd5f392bf
asouil <amaella.souil@gmail.com>
2019-04-08 11:04:16 +0200

    correction

--- index.php
+++ index.php
@@ -7,11 +7,24 @@
 
 <?php 
 	$mois=array("janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre");
+	/* $mois=["janvier",
+			  "février",
+			  "mars",
+			  "avril",
+			  "mai",
+			  "juin",
+			  "juillet",
+			  "aout",
+			  "septembre",
+			  "octobre",
+			  "novembre",
+			  "décembre"] */
 
 	//	echo($mois[2]);
 	//	echo($mois[5]);
 	$mois[7]="août";
-	for($i=0;$i<count($mois);$i++){
+
+	for($i=0; $i < count($mois);$i++){
 	//	if($i==2||$i==5){
 	//		print($mois[$i]."<br/>");
 	//	}
@@ -21,12 +34,21 @@
 	print("<br/>");
 
 	$hautDeFrance=array("02"=>"Aisne","59"=>"Nord","60"=>"Oise","62"=>"Pas-De-Calais","80"=>"Somme");
-//	echo($hautDeFrance[59]);
-	$hautDeFrance[51]="Marne";
-	foreach($hautDeFrance as $key => $val) {
-        print("Le département ".$val." a le numéro ".$key."<br/>");
-    }
-	
+	/*$hautDeFrance=["02"=>"Aisne",
+					 "59"=>"Nord",
+					 "60"=>"Oise",
+					 "62"=>"Pas-De-Calais",
+					 "80"=>"Somme"]; */
+	//	echo($hautDeFrance["59"]);
+	// attention, si on entre en chaîne de caractères le numéro, on l'appelle en chaîne de caractères de préférence.
+	//array_push() sert à insérer une valeur à la fin d'un tableau
+
+	$hautDeFrance["51"]="Marne";
+	//var_dump(exp); die() << n'exécute pas la suite
+
+	foreach($hautDeFrance as $numero => $departement) {
+		print("Le département ".$departement." a le numéro ".$numero.". <br/>");
+	}
 ?>
 
 </body>
